991b967f65d5c2dbc27bc6b8f7571a54f5e898d2
Le0n <leon.listo@hotmail.com>
2021-01-04 11:15:25 +0100

    update and fixes

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,120 +1,209 @@
 <template>
-<article class="card" v-bind:class="['card', vendor]" @click="clickCard()">
- <div class="wrapper">
- <div class="bitcoin" ref="bitcoin" v-bind="changeCard">
-    <img class="chip" src="@/assets/chip-dark.svg">
-    <img class="bitcoin-logo" src="@/assets/vendor-bitcoin.svg">
-    <p class="card-number">{{number}}</p>
-    <p class="cardholder">{{name}}</p>
-    <p class="cardholder">{{expiration}}</p>
-  </div>
- </div>
-</article>
+  <article class="card" v-on:click="pushCard()" v-bind:class="['card', vendor, chip]">
+    <div>
+      <img class="chip-pic" v-bind:src="require(`../assets/chip-${chip}.svg`)">
+      <img class="vendor-pic" v-bind:src="require(`../assets/vendor-${vendor}.svg`)">
+    </div>
+
+    <div>
+      <p class="cardnumber"> {{ cardnumber }} </p>
+    </div>
+
+    <div>
+      <p class="cardholder-text">Cardholder Name</p>
+      <p class="cardholder"> {{ cardholder }} </p>
+    </div>
+
+    <div>
+      <p class="validity-text">Valid thru</p>
+      <p class="validity"> {{ validity }} </p>
+    </div>
+  </article>
 </template>
 
 <script>
 export default {
-  name: 'Card',
-  data: () => {
-    /* default inputs for card */
-    return {
-      defaultNumber: 'xxxx xxxx xxxx xxxx',
-      defaultName: 'Firstname Lastname',
-      defaultExpiration: 'MM/YY',
-      defaultVendor: '',
-      ccv: ''
-    }
-  },
+  name: "Card",
   props: {
     card: Object
   },
-  methods: {
-    clickCard () {
-      this.$emit('clickCard', this.card)
-    }
+  data: () => {
+    return {
+      defaultVendor: "",
+      defaultNumber: "xxxx xxxx xxxx xxxx",
+      defaultHolder: "Firstname/Lastname",
+      defaultValidity: "MM/YY"
+    };
   },
   computed: {
-    number () {
-      if (!this.card.number) {
-        return this.defaultNumber
+    cardnumber() {
+      if (!this.card.cardnumber) {
+        return this.defaultNumber;
       } else {
-        let cardNumber = this.card.number
-        cardNumber = cardNumber.match(/.{1,4}/g)
-        return cardNumber.join(' ')
+        let number = this.card.cardnumber
+        number = number.match(/.{1,4}/g)   
+        return number.join(' ');
       }
     },
-    name () {
-      if (!this.card.name) {
-        return this.defaultName
+    cardholder() {
+      if (!this.card.cardholder) {
+        return this.defaultHolder;
       } else {
-        return this.card.name
+        return this.card.cardholder;
       }
     },
-    expiration () {
-      if (!this.card.expiration) {
-        return this.defaultExpiration
+    validity() {
+      if (!this.card.validity) {
+        return this.defaultValidity;
       } else {
-        return this.card.expiration
+        return this.card.validity;
       }
     },
-    vendor () {
+    vendor() {
       if (!this.card.vendor) {
-        return this.defaultVendor
+        return this.defaultVendor;
       } else {
-        return this.card.vendor
+        return this.card.vendor;
       }
+    },
+    chip() {
+      let chip = "dark";
+      if (!this.card.vendor) {
+        return chip;
+      } else if (this.card.vendor === "bitcoin") {
+        return chip;
+      } else {
+        chip = "light";
+        return chip;
+      }
+    }
+  },
+  methods: {
+    pushCard() {
+      this.$emit("pushCard", this.card)
     }
   }
 }
 </script>
 
 <style scoped>
-*{
-  padding: 0;
-  box-sizing: border-box;
-}
-.card{
+  .card {
     position: absolute;
     width: 382px;
     height: 241px;
-    left: 16px;
+    left: 400px;
     top: 150px;
-}
-.bitcoin{
-  display: flex;
+    display: flex;
+    flex-wrap: wrap;
   justify-content: center;
-  border-style: solid;
-  border-color: orange;
-  padding: 5%;
-  background-color: #FFAE34;
-  border-radius: 8px;
-  margin-left: 25%;
-  margin-right: 25%;
-  padding-bottom: 10%;
-  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
-}
-.bitcoin-logo{
-  position: relative;
-  left: 70%;
-  bottom: 30px;
-}
-.chip{
-  position: relative;
-  right: 10%;
-  bottom: 20px;
-}
-.card-number{
-  position: relative;
-  top: 60px;
-  right: 5%;
-  font-size: 29px;
-  word-spacing: 20px;
-}
-.cardholder{
-position: absolute;
-text-transform: uppercase;
-left: 28%;
-bottom: 23%;
-font-size: 12px;
-}
+  }
+  
+  .chip-pic {
+    position: absolute;
+    width: 70px;
+    height: 60px;
+    left: 20px;
+    top: 20px;
+  }
+  .vendor-pic  {
+    position: absolute;
+    left: 80%;
+    right: 10%;
+    top: 14%;
+  }
+  .bitcoin {
+    background: linear-gradient(rgba(255, 255, 255, 0.15), rgba(255, 255, 255, 0)), #FFAE34;
+    border-radius: 8px;
+    color: #000000;
+  }
+  .ninja {
+    background: linear-gradient( rgba(255, 255, 255, 0.15), rgba(255, 255, 255, 0)), #222222;
+    border-radius: 8px;
+    color: #FFFFFF;
+  }
+  .blockchain {
+    background: linear-gradient( rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0) 100%), #8B58F9;
+    border-radius: 8px;
+    color: #000000;
+  }
+  .evil {
+    background: linear-gradient( rgba(0, 0, 0, 0.16), rgba(0, 0, 0, 0) 100%), #F33355;
+    border-radius: 2px;
+    color: #FFFFFF;
+  }
+  .dark {
+    color: #222222;;
+    text-shadow: -0.5px 0.5px 0px #3a3a3a;
+  }
+  .light {
+    color: #FFFFFF;
+    text-shadow: 0.5px 0.5px 0px #868686, -0.5px -0.5px 0px #868686;
+  }
+  .cardnumber {
+    position: absolute;
+    width: 350px;
+    height: 42px;
+    left: 55px;
+    top: 80px;
+    font-size: 29px;
+    line-height: 32px;
+    letter-spacing: 0.03em;
+  }
+  .cardholder-text {
+    position: absolute;
+    width: 251px;
+    height: 16px;
+    left: 32px;
+    top: 155px;
+    font-size: 15px;
+    line-height: 13px;
+    display: flex;
+    align-items: center;
+    text-transform: uppercase;
+    color: rgba(128, 128, 128, 0.8);
+
+  }
+  .cardholder {
+    position: absolute;
+    width: 251px;
+    height: 32px;
+    left: 32px;
+    top: 170px;
+    font-size: 15px;
+    line-height: 20px;
+    display: flex;
+    align-items: center;
+    text-transform: uppercase;
+  }
+  .validity-text {
+    position: absolute;
+    width: 89px;
+    height: 16px;
+    left: 260px;
+    top: 155px;
+    font-size: 15px;
+    line-height: 13px;
+    display: flex;
+    align-items: center;
+    text-align: right;
+    text-transform: uppercase;
+    color: rgba(128, 128, 128, 0.8);
+    
+  }
+  .validity {
+    position: absolute;
+    font-family: 'PT Mono';
+    font-style: normal;
+    font-size: 18px;
+    line-height: 20px;
+    display: flex;
+    align-items: center;
+    text-align: right;
+    text-transform: uppercase;
+    height: 32px;
+    width: 89px;
+    left: 290px;
+    top: 180px;
+
+  }
 </style>
